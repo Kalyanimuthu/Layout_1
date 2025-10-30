fe4a1f31fba65560b6d1f7824890327b7581ae70
Kalyanimuthu <kalyaniram@gmail.com>
2025-10-30 22:54:17 +0530

    first commit

---
+++ the_kids_reading_book/vite.config.js
@@ -0,0 +1,8 @@
+import { defineConfig } from 'vite'
+import react from '@vitejs/plugin-react'
+
+
+export default defineConfig({
+  plugins: [react()],
+  base: '/Internship/the_kids_reading_book/',
+})
